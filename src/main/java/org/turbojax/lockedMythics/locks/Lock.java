9ac340edb283cd2322bdc7911984c5beaf0dd16a
TurboJax <turbojax07@gmail.com>
2026-04-13 16:03:21 -0400

    removing getMaterial from the Lock

--- src/main/java/org/turbojax/lockedMythics/locks/Lock.java
+++ src/main/java/org/turbojax/lockedMythics/locks/Lock.java
@@ -1,10 +1,8 @@
 package org.turbojax.lockedMythics.locks;
 
-import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public interface Lock {
     String getId();
-    Material getMaterial();
     boolean matches(ItemStack item);
 }
